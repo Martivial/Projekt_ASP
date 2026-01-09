82861b1e5a6ea36c34003833ad831978f10bf99f
Martivial <mateuszrev@gmail.com>
2026-01-09 09:57:00 +0100

    Add unit tests #3

---
+++ Tests/AdsControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Projekt_ASP.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    public class AdsControllerTests
+    {
+        [Fact]
+        public void Create_Get_ReturnsView()
+        {
+            var controller = new AdsController(null);
+            var result = controller.Create();
+            Assert.IsType<ViewResult>(result);
+        }
+
+
+        [Fact]
+        public async void Details_NullId_ReturnsNotFound()
+        {
+            // Arrange
+            var controller = new AdsController(null);
+
+            // Act
+            var result = await controller.Details(null);
+
+            // Assert
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+    }
+}
